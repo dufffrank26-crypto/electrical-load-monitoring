93195d62c6ba89596527e0f795f10691b6e89da3
dufffrank26-crypto <dufffrank26@gmail.com>
2026-02-16 09:22:11 -0500

    Appliance.h

--- Appliance.h
+++ Appliance.h
@@ -1,1 +1,20 @@
+#ifndef APPLIANCE_H
+#define APPLIANCE_H
 
+#include <string>
+
+class Appliance {
+private:
+    std::string name;
+    double powerRatingW; // in watts
+    double dailyUsageHours;
+
+public:
+    Appliance(std::string n, double p, double u);
+    double calculateEnergyKwh() const;
+    std::string getName() const;
+    double getPowerRatingW() const;
+    double getDailyUsageHours() const;
+};
+
+#endif // APPLIANCE_H
