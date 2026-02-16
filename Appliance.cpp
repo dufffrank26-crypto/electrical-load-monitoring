58f9632779a60d1692bc4b12485ecb30f88d14bc
dufffrank26-crypto <dufffrank26@gmail.com>
2026-02-16 09:23:18 -0500

    Appliance.cpp

--- Appliance.cpp
+++ Appliance.cpp
@@ -1,1 +1,12 @@
+#include "Appliance.h"
 
+Appliance::Appliance(std::string n, double p, double u) 
+    : name(n), powerRatingW(p), dailyUsageHours(u) {}
+
+double Appliance::calculateEnergyKwh() const {
+    return (powerRatingW * dailyUsageHours) / 1000.0;
+}
+
+std::string Appliance::getName() const { return name; }
+double Appliance::getPowerRatingW() const { return powerRatingW; }
+double Appliance::getDailyUsageHours() const { return dailyUsageHours; }
